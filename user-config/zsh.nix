655ba6dd770d5370b3610aa1e32bf143b4852266
Tales Cione <talesj@gmail.com>
2023-05-19 15:58:11 +0200

    move swayidle and waybar to systemd

--- user-config/zsh.nix
+++ user-config/zsh.nix
@@ -17,6 +17,7 @@
       if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
         eval $(gnome-keyring-daemon -sd)
         export SSH_AUTH_SOCK=~/.1password/agent.sock
+        systemctl --user import-environment PATH
 
         exec Hyprland
       fi
