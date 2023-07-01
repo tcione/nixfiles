2c350f975c5a13e3cda686184338b245fcfa8588
Tales Cione <talesj@gmail.com>
2023-07-01 12:04:07 +0200

    update packages

--- user-config/gtk.nix
+++ user-config/gtk.nix
@@ -4,7 +4,7 @@
   gtk = {
     enable = true;
     theme = {
-      name = "Catppuccin-Mocha-Standard-Peach-Dark";
+      name = "Catppuccin-Mocha-Standard-Peach-dark";
       package = pkgs.catppuccin-gtk.override {
         accents = [ "peach" ];
         size = "standard";
