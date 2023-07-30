85d0e09d09141c5cdc759d6b9e25258f5b20ad35
Tales Cione <talesj@gmail.com>
2023-07-30 12:15:23 +0200

    fonts.fonts -> fonts.packages

--- configuration.nix
+++ configuration.nix
@@ -139,7 +139,7 @@ in
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
 
-  fonts.fonts = with pkgs; [
+  fonts.packages = with pkgs; [
     font-awesome
     fira
     fira-code
