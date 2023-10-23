00da03a73ca128748ec0f61c0deea6747664f368
Tales Cione <talesj@gmail.com>
2023-10-23 02:54:25 +0200

    add luasnipt to neovim

--- user-config/neovim.nix
+++ user-config/neovim.nix
@@ -19,6 +19,8 @@
       nil
       gnumake
       clang-tools
+      go
+      nodePackages_latest.vscode-langservers-extracted
     ];
 
     plugins = with pkgs.vimPlugins; [
@@ -127,7 +129,9 @@
 
           -- Enable LSP servers
           local capabilities = vim.lsp.protocol.make_client_capabilities()
+          capabilities.textDocument.completion.completionItem.snippetSupport = true
           capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
 
           nvim_lsp.solargraph.setup({
             cmd = { "solargraph", "stdio" },
@@ -155,6 +159,24 @@
             on_attach = on_lsp_attach,
             capabilities = capabilities,
           })
+          nvim_lsp.gopls.setup({
+            on_attach = on_lsp_attach,
+            capabilities = capabilities,
+          })
+          nvim_lsp.html.setup({
+            on_attach = on_lsp_attach,
+            capabilities = capabilities,
+          })
+        '';
+      }
+
+      # Snippets
+      friendly-snippets
+      {
+        plugin = luasnip;
+        type = "lua";
+        config = ''
+          require("luasnip.loaders.from_vscode").lazy_load()
         '';
       }
 
@@ -162,13 +184,26 @@
       cmp-buffer
       cmp-nvim-lsp
       cmp-path
+      cmp_luasnip
       {
         plugin = nvim-cmp;
         type = "lua";
         config = ''
+          local cmp_has_words_before = function()
+            unpack = unpack or table.unpack
+            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+            return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+          end
+
           local cmp = require('cmp')
+          local luasnip = require('luasnip')
 
           cmp.setup({
+            snippet = {
+              expand = function(args)
+                require('luasnip').lsp_expand(args.body)
+              end,
+            },
             mapping = {
               ["<C-k>"] = cmp.mapping.select_prev_item(),
               ["<C-j>"] = cmp.mapping.select_next_item(),
@@ -176,12 +211,33 @@
               ["<C-f>"] = cmp.mapping.scroll_docs(4),
               ["<C-Space>"] = cmp.mapping.complete(),
               ["<C-e>"] = cmp.mapping.close(),
-              ["<Tab>"] = cmp.mapping.confirm({
+              ["<C-CR>"] = cmp.mapping.confirm({
                 behavior = cmp.ConfirmBehavior.Replace,
                 select = true,
               }),
+              ["<Tab>"] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                  cmp.confirm()
+                elseif luasnip.expand_or_jumpable() then
+                  luasnip.expand_or_jump()
+                elseif cmp_has_words_before() then
+                  cmp.complete()
+                else
+                  fallback()
+                end
+              end, { "i", "s" }),
+              ["<S-Tab>"] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                  cmp.confirm()
+                elseif luasnip.jumpable(-1) then
+                  luasnip.jump(-1)
+                else
+                  fallback()
+                end
+              end, { "i", "s" }),
             },
             sources = {
+              { name = 'luasnip' },
               { name = 'nvim_lsp' },
               { name = 'path' },
               { name = 'buffer' },
